27503784cc477ab62b5b724428ae3a1fad5e7bb5
aestes3 <aidanestes@gmail.com>
2024-08-20 16:54:54 -0400

    Created function and boxplots for regression model and tables

--- R/WIP_final.R
+++ R/WIP_final.R
@@ -1,24 +1,74 @@
 library(tidyverse)
 library(gtsummary)
+library(here)
+library(janitor)
+library(Hmisc)
+library(summarytools)
+library(ggplot2)
 
 #Import Data
 raw_data <- readRDS(here::here("data/raw/raw_data.rds"))
-#Drop unnecessary variables
+#Drop unnecessary variables and NA
 edited <- raw_data |>
-										subset(select = c(subject_id, clinic_name, gender, test_id, result, drive_thru_ind,
-													 col_rec_tat, rec_ver_tat))
-
+										select(c(subject_id, age, clinic_name,
+																			gender, test_id, result, drive_thru_ind,
+																			col_rec_tat, rec_ver_tat)) |>
+   na.omit() |>
 
 #Label data
-data = edited
-mutate(drive_thru_ind = factor(drivethru, labels = c("No", "Yes")))
+		mutate(drive_thru_ind = factor(drive_thru_ind,
+																	 levels = c(0,1),
+																	 labels = c("No", "Yes")))
+
+
+
+
+label(edited$col_rec_tat) <- "Time (hours) between collection and lab received"
+label(edited$rec_ver_tat) <- "Time (hours) between received and test verification"
 
 #Create gtsummary table (table1)
 table1 <- tbl_summary(
-	raw_data,
+	edited,
 	by = gender,
 	include = c(age, result, drive_thru_ind, col_rec_tat,
 							rec_ver_tat)) |>
 	add_overall(last = TRUE)
+
+summary(edited$age)
 table1
+
+#Create regression
+tbl_uvregression(
+	edited,
+	y = age,
+	include = c(result, gender, clinic_name),
+	method = lm)
+
+#Create figure
+library(ggplot2)
+
+#Boxplot with color coded results
+p <- ggplot(data=edited, aes(x=result, y=age, fill= result)) +
+		geom_boxplot() +
+	scale_fill_manual(values=c("grey", "red", "green")) + #adds manual colors in order of result
+	theme_minimal() #adds white background
+p
+
+
+#Boxplot creation function
+create_boxplot <- function(var){
+	temp <- ggplot(data=edited, aes(x=var, y=age, fill=var)) +
+		geom_boxplot() +
+		theme_minimal()
+	return(temp)
+
+
+}
+
+create_boxplot(edited$gender)
+
+#ggplot(data=edited, aes(result)) +
+	#	geom_bar()
+#table(edited$result)
+
 
